8c3a7ce4cc394fa21c26a7f820f2f8bf8be956bb
j <j@js-MacBook-Pro.local>
2016-09-01 02:05:41 -0500

    Push

---
+++ gulpfile.js
@@ -0,0 +1,108 @@
+var gulp = require('gulp'),
+sass = require('gulp-sass'),
+autoprefixer = require('gulp-autoprefixer'),
+globbing = require('gulp-css-globbing'),
+sourcemaps = require('gulp-sourcemaps'),
+imagemin = require('gulp-imagemin'),
+flatten = require('gulp-flatten'),
+newer = require('gulp-newer'),
+concat = require('gulp-concat'),
+uglify = require('gulp-uglify'),
+connect = require('gulp-connect'),
+open = require('gulp-open'),
+svg = require('gulp-svg-sprite'),
+fileinclude = require('gulp-file-include'),
+livereload = require('gulp-livereload'),
+filter    = require('gulp-filter'),
+gutil = require('gulp-util'),
+path = require('path');
+
+gulp.task('css', function() {
+  return gulp.src('dev/sass/application.scss')
+  .pipe(sourcemaps.init())
+  .pipe(globbing({extensions: '.scss'}))
+  .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+  .pipe(autoprefixer({cascade: false}))
+  .pipe(sourcemaps.write())
+  .on('error', handleError)
+  .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('vendor-js', function() {
+  return gulp.src('dev/js/vendor/*.js')
+  .pipe(sourcemaps.init())
+  .pipe(concat('application-vendor.js'))
+  .pipe(uglify())
+  .pipe(sourcemaps.write())
+  .on('error', handleError)
+  .pipe(gulp.dest('public/js'));
+});
+
+gulp.task('js', function() {
+  return gulp.src(['dev/js/*.js',  'dev/js/**/*.js', '!dev/js/vendor/*.js'])
+  .pipe(sourcemaps.init())
+  .on('error', handleError)
+  .pipe(concat('application.js'))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest('public/js'));
+});
+
+gulp.task('img', function() {
+  return gulp.src('dev/img/**/*.{jpg,jpeg,png,gif,ico,svg}')
+  .pipe(flatten())
+  .pipe(newer('public/img'))
+  .pipe(imagemin({
+    optimizationLevel: 5,
+    progressive: true,
+    interlaced: true,
+    svgoPlugins: []
+  }))
+  .on('error', handleError)
+  .pipe(gulp.dest('public/img'));
+});
+
+gulp.task('html', function() {
+  return gulp.src('dev/html/*.html')
+  .pipe(fileinclude({
+    prefix: '@@',
+    basepath: '@file'
+  }))
+  .on('error', handleError)
+  .pipe(newer('dev/html/**/*.html'))
+  .on('error', handleError)
+  .pipe(gulp.dest('public/'));
+});
+
+gulp.task('open', function(){
+  gulp.src('')
+  .pipe(open({ uri: 'http://localhost:8080'}));
+});
+
+gulp.task('connect', function() {
+  gulp.watch('dev/sass/**/**/*.scss', ['css']);
+  gulp.watch('dev/js/vendor/*.js', ['vendor-js']);
+  gulp.watch(['dev/js/**/*.js', '!dev/js/vendor/*.js'], ['js']);
+  gulp.watch('dev/img/**/*.{jpg,jpeg,png,gif,ico,svg}', ['img']);
+  gulp.watch('public/img/**/*.svg', ['html']);
+  gulp.watch('public/html/**/*.html', ['html']);
+
+  livereload.listen();
+
+  gulp.watch(['public/*.html', 'public/js/*.js', 'dev/img/**/*.svg', '.{jpg,jpeg,png,gif,ico}', 'public/css/*.css']).on('change', livereload.changed);
+
+  connect.server({
+    root: 'public/',
+    // host: '',
+    keepalive: false,
+    livereload: true
+  });
+
+});
+
+// Error reporting function
+function handleError(err) {
+  console.log(err.toString());
+  this.emit('end');
+}
+
+gulp.task('default', ['css', 'vendor-js', 'js', 'img', 'html', 'connect', 'open']);
